217df09264aadbfb48fa78663d876a5e58288204
Dimiter Vakovsky <vakovsky@gmail.com>
2022-04-18 17:10:21 +0300

    Update ProgramCF.cs

--- ProgramCF.cs
+++ ProgramCF.cs
@@ -1,1 +1,58 @@
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            //INSERT
+            using (var context = new DataBaseContext())
+            {
+                var user = new User
+                {
+                    Name = "User1",
+                    Email = "user1@site.com",
+                    Age = 18,
+                };
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
 
+            //SELECT
+            using (var context = new DataBaseContext())
+            {
+                var user = (from d in context.Users
+                            where d.Name == "User1"
+                            select d).Single();
+                Console.WriteLine(user.Name);
+            }
+
+            //UPDATE
+            using (var context = new DataBaseContext())
+            {
+                var user = (from d in context.Users
+                               where d.Name == "User1"
+                               select d).Single();
+                user.Name = "User2";
+                context.SaveChanges();
+            }
+
+            //DELETE
+            using (var context = new DataBaseContext())
+            {
+                var bay = (from d in context.Users where d.Name == "User2" select d).Single();
+                context.Users.Remove(bay);
+                context.SaveChanges();
+            }
+
+            //SELECT ALL
+            using (var context = new DataBaseContext())
+            {
+                var users = from b in context.Users
+                            orderby b.Name
+                            select b;
+                Console.WriteLine("All users in the database:");
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
