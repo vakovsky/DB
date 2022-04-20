83900a4a9dfc4dd33740d9ed835315c62dd09765
Dimiter Vakovsky <vakovsky@gmail.com>
2022-04-20 16:55:48 +0300

    Update DataBaseContext.cs

--- DataBaseContext.cs
+++ DataBaseContext.cs
@@ -13,10 +13,21 @@ namespace ConsoleApp1
         public string Name { get; set; }
         public string Email { get; set; }
         public int Age { get; set; }
+        
+        public virtual List<Log> Logs { get; set; }
+    }
+    public class Log
+    {
+        public int LogId { get; set; }
+        public string DateTime Date { get; set; }
+            
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
     }
     public class DataBaseContext : DbContext
     {
         public DataBaseContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=Database2.mdf;Integrated Security=True;Connect Timeout=30") { }
         public DbSet<User> Users { get; set; }
+        public DbSet<Log> Logs { get; set; }
     }
 }
