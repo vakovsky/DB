e38af42c6884b31b2e052987a4d691365f35cb89
Dimiter Vakovsky <vakovsky@gmail.com>
2022-04-27 17:48:32 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -7,9 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.Entity;
+using ClassLibraryDB;
 
-namespace WindowsFormsApp4
+namespace WindowsFormsAppDB
 {
     public partial class Form1 : Form
     {
@@ -20,45 +20,92 @@ namespace WindowsFormsApp4
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //SELECT ALL
+            using (var context = new DataBaseContext())
+            {
+                comboBox1.Items.Clear();
+                dataGridView1.Rows.Clear();
+                var users = from u in context.Users
+                            orderby u.Name
+                            select u;
+                foreach (var user in users)
+                {
+                    comboBox1.Items.Add(user.UserId);
+                    dataGridView1.Rows.Add(user.UserId, user.Name, user.Email, user.Age);
+                }
+                comboBox1.SelectedIndex = 0;
+            }         
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (var context = new UniDbContext())
+            //INSERT
+            User user = new User();
+
+            user.Name = this.textBox1.Text;
+            user.Email = this.textBox2.Text;
+            user.Age = int.Parse(this.textBox3.Text);
+
+            using (var context = new DataBaseContext())
             {
-                var user = new User
-                {
-                    Name = this.textBox1.Text,
-                    Email = this.textBox2.Text,
-                    Age = int.Parse(this.textBox3.Text),
-                };
                 context.Users.Add(user);
                 context.SaveChanges();
             }
+            comboBox1.Items.Clear();
+            Form1_Load(this, e);
         }
-    }
-    public class User //Колони в таблицата
-    {
-        public int UserId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public int Age { get; set; }
-        public virtual List<Log> Logs { get; set; }
-    }
-    public class Log
-    {
-        public int LogId { get; set; }
-        public string Info { get; set; }
 
-        public int UserId { get; set; }
-        public virtual User User { get; set; }
-    }
-    public class UniDbContext : DbContext
-    {
-        //public UniDbContext() : base("Uni") { }
-        public UniDbContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Teacher412\Documents\dataef.mdf;Integrated Security=True;Connect Timeout=30") { }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Log> Logs { get; set; }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            using (var context = new DataBaseContext())
+            {
+                int id = int.Parse(this.comboBox1.Text);
+                var user = (from u in context.Users
+                            where u.UserId == id
+                            select u).Single();
+                textBox1.Text = user.Name;
+                textBox2.Text = user.Email;
+                textBox3.Text = user.Age.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //UPDATE
+            using (var context = new DataBaseContext())
+            {
+                int id = int.Parse(this.comboBox1.Text);
+                var user = (from s in context.Users
+                            where s.UserId == id
+                            select s).Single();
+                user.Name = this.textBox1.Text;
+                user.Email = this.textBox2.Text;
+                user.Age = int.Parse(this.textBox3.Text);
+                context.SaveChanges();
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //DELETE
+            using (var context = new DataBaseContext())
+            {
+                int id = int.Parse(this.comboBox1.Text);
+                var user = (from s in context.Users 
+                           where s.UserId == id
+                           select s).Single();
+                context.Users.Remove(user);
+                context.SaveChanges();
+            }
+            comboBox1.Items.Clear();
+            Form1_Load(this, e);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            button2_Click(sender, e);
+        }
     }
 }
